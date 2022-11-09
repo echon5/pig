e3635de2a9416799b1318ce4c58b63bde4a5a275
Erin Chon <echon@bsd220.org>
2022-11-09 09:54:16 -0600

    created initial view

--- pig/ContentView.swift
+++ pig/ContentView.swift
@@ -6,16 +6,25 @@
 //
 
 import SwiftUI
-
 struct ContentView: View {
+    @State private var turnScore = 0
+    @State private var gameScore = 0
+    @State private var randomValue = 0
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Color.gray.opacity(0.7).ignoresSafeArea()
+            VStack {
+                Image("Pig").resizable().frame(width: 150, height: 150)
+                Text("Dice Roll").font(.title)
+                Text("\(randomValue)")
+                .font(.system(size: 72))
+                .padding()
+                .onTapGesture {
+                    randomValue = Int.random(in:1...6)
+                }
+                Spacer ()
+            }
         }
-        .padding()
     }
 }
 
