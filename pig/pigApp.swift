4f821be405c4fc43429ac05a6632c93d10f38836
Erin Chon <echon@bsd220.org>
2022-11-09 09:28:54 -0600

    Initial Commit

---
+++ pig/pigApp.swift
@@ -0,0 +1,17 @@
+//
+//  pigApp.swift
+//  pig
+//
+//  Created by Erin Chon on 11/9/22.
+//
+
+import SwiftUI
+
+@main
+struct pigApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
