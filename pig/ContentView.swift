b2d1236f783d9d853804b50a7abb150ac22b562e
Erin Chon <echon@bsd220.org>
2022-11-10 10:06:03 -0600

    added die image disregard last commit

--- pig/ContentView.swift
+++ pig/ContentView.swift
@@ -10,21 +10,30 @@ struct ContentView: View {
     @State private var turnScore = 0
     @State private var gameScore = 0
     @State private var randomValue = 0
+    @State private var rotation = 0.0
     var body: some View {
         ZStack {
             Color.gray.opacity(0.7).ignoresSafeArea()
             VStack {
                 Image("Pig").resizable().frame(width: 150, height: 150)
-                Text("Dice Roll").font(.title)
-                Text("\(randomValue)")
-                .font(.system(size: 72))
-                .padding()
-                .onTapGesture {
-                    randomValue = Int.random(in:1...6)
-                }
+                CustomText(text: "Pig")
+                Image ("pips \(randomValue)")
+                    .resizable()
+                    .frame(width: 150, height: 150)
+                    .rotationEffect(.degrees(rotation))
+                    .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z:0))
+                    .padding(50)
+                CustomText(text: "Turn Score: \(turnScore)")
+                CustomText(text: "Game Score: \(gameScore)")
                 Spacer ()
             }
         }
+    }
+}
+struct CustomText: View {
+    let text: String
+    var body: some View {
+        Text(text).font(Font.custom ("Marker Felt", size: 36))
     }
 }
 
