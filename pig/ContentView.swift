373b335310b1eb8f3e1c4b1331051a46e092c7c0
Erin Chon <echon@bsd220.org>
2022-11-15 09:30:55 -0600

    added game instructions

--- pig/ContentView.swift
+++ pig/ContentView.swift
@@ -12,31 +12,69 @@ struct ContentView: View {
     @State private var randomValue = 0
     @State private var rotation = 0.0
     var body: some View {
-        ZStack {
-            Color.gray.opacity(0.7).ignoresSafeArea()
-            VStack {
-                Image("Pig").resizable().frame(width: 150, height: 150)
-                CustomText(text: "Pig")
-                Image ("pips \(randomValue)")
-                    .resizable()
-                    .frame(width: 150, height: 150)
-                    .rotationEffect(.degrees(rotation))
-                    .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z:0))
-                    .padding(50)
-                CustomText(text: "Turn Score: \(turnScore)")
-                HStack {
-                    Button ("Roll") {
+        NavigationView {
+            ZStack {
+                Color.gray.opacity(0.7).ignoresSafeArea()
+                VStack {
+                    Image("Pig").resizable().frame(width: 150, height: 150)
+                    CustomText(text: "Pig")
+                    Image ("pips \(randomValue)")
+                        .resizable()
+                        .frame(width: 150, height: 150)
+                        .rotationEffect(.degrees(rotation))
+                        .rotation3DEffect(.degrees(rotation), axis: (x: 1, y: 1, z:0))
+                        .padding(50)
+                    CustomText(text: "Turn Score: \(turnScore)")
+                    HStack {
+                        Button ("Roll") {
+                            chooseRandom(times: 3)
+                            withAnimation(.interpolatingSpring(stiffness: 10, damping: 2)) {
+                                rotation += 360
+                            }
+                        }
+                        .buttonStyle(CustomButtonStyle())
+                        Button("Hold") {
+                            gameScore += turnScore
+                            endTurn()
+                            withAnimation(.easeInOut(duration: 1 )) {
+                                rotation += 360
+                            }
+                        }
+                        .buttonStyle(CustomButtonStyle())
                     }
-                    .buttonStyle(CustomButtonStyle())
-                    Button("Hold") {
-                    }
-                    .buttonStyle(CustomButtonStyle())
-                    }
-                CustomText(text: "Game Score")
-                
-                CustomText(text: "Game Score: \(gameScore)")
+                    CustomText(text: "Game Score")
+                    CustomText(text: "Game Score: \(gameScore)")
+                    Spacer ()
+                    NavigationLink("How to play", destination: InstructionsView())
+                        .font(Font.custom("Marker Felt", size: 24))
+                        .padding()
+                }
             }
-            Spacer ()
+        }
+    }
+    
+    
+    func endTurn() {
+        turnScore = 0
+        randomValue = 0
+    }
+    
+    func chooseRandom (times: Int) {
+        if times > 0 {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                randomValue = Int.random(in: 1...6)
+                chooseRandom(times: times - 1)
+            }
+        }
+        if times == 0 {
+            if randomValue == 1 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1) {
+                    endTurn ()
+                }
+            }
+            else {
+                turnScore += randomValue
+            }
         }
     }
     
@@ -50,16 +88,43 @@ struct ContentView: View {
                 .clipShape(RoundedRectangle(cornerRadius: 10))
         }
     }
+    
+    struct CustomText: View {
+        let text: String
+        var body: some View {
+            Text(text).font(Font.custom ("Marker Felt", size: 36))
+        }
+    }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
 }
-struct CustomText: View {
-    let text: String
+struct InstructionsView: View {
     var body: some View {
-        Text(text).font(Font.custom ("Marker Felt", size: 36))
+        ZStack{
+            Color.gray.opacity(0.7).ignoresSafeArea()
+            VStack {
+                Image("Pig").resizable().frame(width: 150, height: 150)
+                Text("Pig").font(.title)
+                VStack (alignment: .leading){
+                    Text("In the game of pig, players take individual turns. Each turn, a player repeatly rolls a single die until either is a pig or the player decides to \"hold\".")
+                        .padding()
+                    Text ("If the player rolls a pig, they score nothing, and it becomes the next players turn.")
+                        .padding()
+                    Text ("If the player rolls any other number, it is added to their total, and the players turn continues.")
+                        .padding()
+                    
+                    Text ("If a player chooses to \"hold\", their total is added to their game score, and it becomes the next players turn.")
+                        .padding()
+                    Text("A player wins the game when the game score becomes 100 or more on their turn.")
+                        .padding()
+                }
+                Spacer ()
+            }
+        }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
